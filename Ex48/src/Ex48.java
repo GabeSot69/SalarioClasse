6f6ccc52d94bfb54bad6f41c0358070e31157ff5
GabeSot69 <105126836+GabeSot69@users.noreply.github.com>
2022-08-15 13:47:10 -0400

    Add files via upload

---
+++ Ex48/src/Ex48.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ex48 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o salario");
+        double salario = scanner.nextDouble();
+        System.out.println("Digite a classe");
+        scanner.nextLine();
+        String classe = scanner.nextLine();
+        double aumento = 0;
+        switch (classe){
+            case "A":
+                aumento = 0.15;
+                break;
+
+            case "B":
+                aumento = 0.2;
+                break;
+
+            case "C":
+                aumento = 0.3;
+                break;
+
+            default:
+                System.out.println("Invalido");
+                break;
+        }
+        System.out.println("Aumento: " + aumento*100 + " %");
+        System.out.println("Salario: R$ " + salario*(1+aumento));
+
+    }
+}

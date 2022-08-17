33a2dccffe309f0b6b058090ee1e4d3154e12505
GabeSot69 <105126836+GabeSot69@users.noreply.github.com>
2022-08-17 08:52:36 -0400

    Add files via upload

--- Ex48/src/Ex48.java
+++ Ex48/src/Ex48.java
@@ -1,8 +1,10 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ex48 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
         System.out.println("Digite o salario");
         double salario = scanner.nextDouble();
         System.out.println("Digite a classe");
@@ -27,7 +29,7 @@ public class Ex48 {
                 break;
         }
         System.out.println("Aumento: " + aumento*100 + " %");
-        System.out.println("Salario: R$ " + salario*(1+aumento));
+        System.out.println("Salario: R$ " + df.format(salario*(1+aumento)));
 
     }
 }
